a4edc47c13a4b12cda9461252b85253a3da6364f
joonullus <ceyhun.denizkeles@gmail.com>
2024-02-08 11:15:18 +0300

    Completed task C (methods to find maximum and minimum values).

--- github_assignment.java
+++ github_assignment.java
@@ -2,4 +2,44 @@ public class github_assignment {
     public static void main(String[] args) {
         
     }
+
+    /**
+     * This method finds the number with the maximum value in a given array of integers.
+     * @param numbers
+     * @return max number
+     */
+    public static int findMaximum (int[] numbers)
+    {
+        int maxSoFar = numbers[0]; // Taking the number in the first index as the max at start
+
+        for (int arrayIndex = 1; arrayIndex < numbers.length; arrayIndex++)
+        {
+            if (maxSoFar < numbers[arrayIndex])
+            {
+                maxSoFar = numbers[arrayIndex];
+            }
+        }
+
+        return maxSoFar;
+    }
+
+    /**
+     * This method finds the number with the minimum value in a given array of integers.
+     * @param numbers
+     * @return min number
+     */
+    public static int findMinimum (int[] numbers)
+    {
+        int minSoFar = numbers[0]; // Taking the number in the first index as the min at start
+
+        for (int arrayIndex = 1; arrayIndex < numbers.length; arrayIndex++)
+        {
+            if (minSoFar > numbers[arrayIndex])
+            {
+                minSoFar = numbers[arrayIndex];
+            }
+        }
+
+        return minSoFar;
+    } 
 }

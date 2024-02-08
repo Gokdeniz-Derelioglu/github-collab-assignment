769fdafad3ab5752087c36b8bfe25c47ee8e3356
Gokdeniz-Derelioglu <derelioglugokdeniz@gmail.com>
2024-02-08 11:15:49 +0300

    Created Method for Array
    Added method for creating array of random numbers between (0,100).

--- github_assignment.java
+++ github_assignment.java
@@ -1,5 +1,25 @@
+import java.util.Random;
+
 public class github_assignment {
     public static void main(String[] args) {
         
+    }
+
+    /** 
+     * This method creates an array of random numbers between (0,100)
+     * @param arraySize, size of the array specified.
+     * @return
+     * Written by Gökdeniz Derelioğlu
+     */
+    private static int[] createRandomArray(int arraySize) {
+        int x;
+        Random random = new Random();
+        int[] newArray = new int[arraySize]; 
+        for(int i = 0; i<= arraySize; i++)
+        {
+            x = random.nextInt(101);
+            newArray[i] = x;
+        }
+        return newArray;
     }
 }
